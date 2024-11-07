a2d6003d20c5bcdaec93212c4dddbd4d3cb313a8
nao <>
2024-11-07 10:10:56 +0900

    対話フォーマットの作成

--- book_project/book_app/views.py
+++ book_project/book_app/views.py
@@ -21,11 +21,20 @@ class ExistQuestion(BaseModel):
     exist: bool
 
 class Content(BaseModel):
+    id: int
     title: str
     description: str
 
 class StructedToc(BaseModel):
     contents: list[Content]
+
+class Question(BaseModel):
+    content_id: int
+    question: str
+    answer: str
+
+class QuestionList(BaseModel):
+    questions: list[Question]
 
 
 
@@ -69,10 +78,13 @@ def upload_pdf(request):
             document.text_content = text_content  # 抽出した文字データを保存
             # summary = summarize_text(text_content, points = points)
             # document.text_summary = summary
-            document.question_list = form.cleaned_data['question_list']
+            # document.question_list = form.cleaned_data['question_list']
             document.question_num = 0
             structed_toc = generate_structed_toc(text_content)
             document.structed_toc = structed_toc.dict()
+
+            questions = generate_structed_questions(structed_toc, document)
+            document.question_list = questions.dict()
 
             # toc = ''
             # for content in structed_toc.contents:
@@ -144,6 +156,24 @@ def find_relevant_sentences(question):
     
     return similar_sentences
 
+def generate_structed_questions(structed_toc, document):
+    questions = []
+    client = OpenAI()
+    messages = [
+            {'role': 'system', 'content': f"日本語でユーザのメッセージは目次とその概要です。目次の各テーマに対する質問とその回答を5個ずつ作成して。ただし、作成する質問に対する答えが以下の文章に含まれるようにして。\n {document.text_content}"}
+        ]
+    for content in structed_toc.contents:
+        messages.append({'role': 'user', 'content': f"id:{content.id} title:{content.title} content.description\n"})
+    print(messages)
+    response = client.beta.chat.completions.parse(
+        model="gpt-4o-mini-2024-07-18",
+        messages=messages,
+        response_format=QuestionList
+    )
+    questions = response.choices[0].message.parsed
+    return questions
+
+
 def generate_structed_toc(text):
     client = OpenAI()
 
@@ -156,6 +186,7 @@ def generate_structed_toc(text):
         response_format=StructedToc
     )
     structed_toc = response.choices[0].message.parsed
+    print(structed_toc)
     return structed_toc
 
 
@@ -227,68 +258,76 @@ def chat_with_ai(request, document_id):
         Interaction.objects.create(document=document, role='user', message=user_message)
 
         if document.question_progress == 0:
-            ai_message = document.question_list[document.question_num]
+            print(document.question_list['questions'])
+            ai_message = document.question_list['questions'][document.question_num]['question']
             Interaction.objects.create(document=document, role='ai', message=ai_message)
             document.question_progress = 1
             document.save()
             return JsonResponse({'response': ai_message})
 
         try:
-            past_interactions = Interaction.objects.filter(document=document).order_by('timestamp')
-            messages = [{"role": "system", "content": f"あなたは専門家です。日本語で出力してください。ただし、回答の際には以下のpdfに書かれていた情報を参考にしてください。{document.text_content}"}]
-            #past_interactionsの中で最も新しいaiのメッセージを取得
-            if len(past_interactions) > 0:
-                ai_last_message = past_interactions.filter(role='ai').last().message
+            if document.question_num > 8:
+                document.question_progress = 0
+                document.question_num = 0
+                document.save()
+                return JsonResponse({'response': '回答の評価と質問の深堀り'})
+            else:
+                past_interactions = Interaction.objects.filter(document=document).order_by('timestamp')
+                messages = [{"role": "system", "content": f"あなたは専門家です。日本語で出力してください。ただし、回答の際には以下のpdfに書かれていた情報を参考にしてください。{document.text_content}"}]
+                #past_interactionsの中で最も新しいaiのメッセージを取得
+                if len(past_interactions) > 0:
+                    ai_last_message = past_interactions.filter(role='ai').last().message
 
-            messages.append({"role": "assistant", "content": ai_last_message})
+                messages.append({"role": "assistant", "content": ai_last_message})
 
-            # for interaction in past_interactions:
-            #     if interaction.role == 'ai':
-            #         interaction.role = 'assistant'
-            #     messages.append({"role": interaction.role, "content": interaction.message})
+                # for interaction in past_interactions:
+                #     if interaction.role == 'ai':
+                #         interaction.role = 'assistant'
+                #     messages.append({"role": interaction.role, "content": interaction.message})
 
-            shortage_question = f'まず最初に質問に対するAIの回答をpdfの内容を基に簡潔に出力してください。それから、以下の内容が正しい内容を述べているか作成した回答と比較して評価してください。また、不足している部分や補足すべきな点があればその内容に関して問うpdf中に答えが存在する質問を一つ作成してください。{user_message}'
+                shortage_question = f'まず最初に質問に対するAIの回答をpdfの内容を基に簡潔に出力してください。それから、以下の内容が正しい内容を述べているか作成した回答と比較して評価してください。また、不足している部分や補足すべきな点があればその内容に関して問うpdf中に答えが存在する質問を一つ作成してください。{user_message}'
 
-            relevant_question = f'まず最初に質問に対するAIの回答をpdfの内容を基に簡潔に出力してください。この時、ユーザの発言は無視してください。それから、以下の内容が正しい内容を述べているか作成した回答と比較して評価してください。また、質問内容、回答内容、pdfの内容に関連のあるpdf中に答えが存在する質問を一つ作成してください。{user_message}'
+                relevant_question = f'まず最初に質問に対するAIの回答をpdfの内容を基に簡潔に出力してください。この時、ユーザの発言は無視してください。それから、以下の内容が正しい内容を述べているか作成した回答と比較して評価してください。また、質問内容、回答内容、pdfの内容に関連のあるpdf中に答えが存在する質問を一つ作成してください。{user_message}'
 
-            # 新しいユーザーメッセージを追加
-            messages.append({"role": "user", "content": shortage_question})
+                # 新しいユーザーメッセージを追加
+                messages.append({"role": "user", "content": shortage_question})
+                
+                client = OpenAI()
+                model="gpt-4o-mini"
+                # ChatGPT APIの呼び出し
+                response = client.chat.completions.create(
+                    model=model,
+                    messages=messages,
+                    # max_tokens=100,  # 要約結果のトークン数の制限
+                    temperature=0.5,  # 出力の多様性の調整
+                    # response_format=Evaluation,
+                )
+                # 返答メッセージの抽出
+                ai_message = response.choices[0].message.content
+                ai_message = markdown.markdown(ai_message)
+                # comprehension_level = ai_message.comprehension_level
+                # not_understand = ai_message.not_understand
+                # ai_message = f"comprehension_level:{comprehension_level}\n not_understand:{not_understand}"
+
+                relevant_sentences = find_relevant_sentences(ai_message)
+                #関連度順にソート
+                relevant_sentences = sorted(relevant_sentences, key=lambda x: x[1], reverse=True)
+
+                sentences = SENTENCE.objects.all().values_list('sentence', flat=True)
+
+                #最も関連度の高いページを取得
+                sentence = relevant_sentences[0][0]
+                ai_message = f"{ai_message}\n\n関連する文章: {sentences[sentence]}"
+
+                # AIのメッセージを保存
+                Interaction.objects.create(document=document, role='ai', message=ai_message)
+                # document.question_progress = 0
+                document.question_num = 9
+                document.save() 
+
+
+                return JsonResponse({'response': ai_message})
             
-            client = OpenAI()
-            model="gpt-4o-mini"
-            # ChatGPT APIの呼び出し
-            response = client.chat.completions.create(
-                model=model,
-                messages=messages,
-                # max_tokens=100,  # 要約結果のトークン数の制限
-                temperature=0.5,  # 出力の多様性の調整
-                # response_format=Evaluation,
-            )
-            # 返答メッセージの抽出
-            ai_message = response.choices[0].message.content
-            ai_message = markdown.markdown(ai_message)
-            # comprehension_level = ai_message.comprehension_level
-            # not_understand = ai_message.not_understand
-            # ai_message = f"comprehension_level:{comprehension_level}\n not_understand:{not_understand}"
-
-            relevant_sentences = find_relevant_sentences(ai_message)
-            #関連度順にソート
-            relevant_sentences = sorted(relevant_sentences, key=lambda x: x[1], reverse=True)
-
-            sentences = SENTENCE.objects.all().values_list('sentence', flat=True)
-
-            #最も関連度の高いページを取得
-            sentence = relevant_sentences[0][0]
-            ai_message = f"{ai_message}\n\n関連する文章: {sentences[sentence]}"
-
-            # AIのメッセージを保存
-            Interaction.objects.create(document=document, role='ai', message=ai_message)
-            # document.question_progress = 0
-            # document.question_num = document.question_num + 1
-            # document.save()
-
-
-            return JsonResponse({'response': ai_message})
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
 
